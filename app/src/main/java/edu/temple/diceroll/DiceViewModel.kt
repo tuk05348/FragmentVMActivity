f3ad56f13caafb348c5c3b05363b1d7d96cbdde9
Niaz Baharudeen <tuk05348@temple.edu>
2024-03-21 11:23:50 -0400

    Filled in DiceViewModel with a selectedSide parameter and a getter and setter for dealing with LiveData.

--- app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
+++ app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
@@ -1,7 +1,17 @@
 package edu.temple.diceroll
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DiceViewModel : ViewModel() {
-    
+    private val selectedSide = MutableLiveData<Int>()
+
+    fun setSelectedSide(side: Int){
+        selectedSide.value = side
+    }
+
+    fun getSelectedSide() : LiveData<Int> {
+        return selectedSide
+    }
 }
